d95e27482a0e04ae9b10fdf77ac92508bf7e84cc
Rafael Grigorian <me@raffi.io>
2018-08-15 11:05:46 -0500

    Fixed GH-3

--- src/nginx/bin/startup.sh
+++ src/nginx/bin/startup.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env sh
 
+if [[ "$MAGENTO_VERSION" != "1" ]] && [[ "$MAGENTO_VERSION" != "2" ]]; then
+	echo "Valid value for MAGENTO_VERSION is not found ... using default value 2"
+	MAGENTO_VERSION="2"
+fi
+
 if [[ "$VERBOSE" == "true" ]]; then
+	mkdir -p /var/log/nginx
+	touch /var/log/nginx/error.log /var/log/nginx/access.log
 	tail -f /var/log/nginx/*.log &
 fi
 
+cp "/etc/nginx/available.d/magento-$MAGENTO_VERSION.conf" "/etc/nginx/conf.d/http.conf"
 chown -R nginx:nginx /var/www
 nginx -g 'daemon off;'
--- src/php-fpm-5.6/bin/startup.sh
+++ src/php-fpm-5.6/bin/startup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 if [[ "$VERBOSE" == "true" ]]; then
-	touch /var/log/php7/error.log
-	tail -f /var/log/php7/*.log &
+	touch var/log/php-fpm.log
+	tail -f /var/log/php-fpm.log &
 fi
 
 chown -R nobody:nobody /var/www
-php-fpm7 -F
+php-fpm5 -F
